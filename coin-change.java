4c777bd50c12f331556d2b02292ca6481c82083a
Lohith Aswathappa <47034766+Lohith-Aswathappa@users.noreply.github.com>
2020-11-26 13:26:03 -0600

    working commit - Created using LeetHub

---
+++ coin-change.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        //edge case
+        if(amount == 0 || coins == null || coins.length == 0)
+            return 0;
+        //logic
+        int[][] dp = new int[coins.length+1][amount+1];
+        
+        for(int i = 0; i < dp.length; i++){
+            dp[i][0] = 0;
+        }
+        
+        for(int i = 0; i < dp[0].length; i++){
+            dp[0][i] = 9999;
+        }
+        
+        for(int i = 1; i < dp.length; i++){
+            for(int j = 1; j < dp[0].length; j++){
+                if(j < coins[i-1]){
+                    dp[i][j] = dp[i-1][j];
+                }else{
+                    dp[i][j] = Math.min(dp[i-1][j], dp[i][j-coins[i-1]] + 1);
+                }
+            }
+        }
+        
+        if (dp[coins.length+1-1][amount] >= 9999) return -1;
+        return dp[dp.length-1][dp[0].length-1];
+    }
+}
