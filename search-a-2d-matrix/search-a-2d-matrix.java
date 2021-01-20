c178a81bf15b1a0613d14fe3f5079d80469326da
Lohith Aswathappa <47034766+Lohith-Aswathappa@users.noreply.github.com>
2021-01-19 19:41:56 -0600

    Update search-a-2d-matrix.java

--- search-a-2d-matrix/search-a-2d-matrix.java
+++ search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,3 +1,5 @@
+//Brute force approach
+
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         //edge case
@@ -15,4 +17,39 @@ class Solution {
         
         return false;
     }
+}
+
+
+//optimized binary search approach
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        //edge case
+        if(matrix == null || matrix.length == 0){
+            return false;
+        }
+        //logic
+        int start = 0;
+        int n = matrix[0].length;
+        int end = matrix.length * matrix[0].length - 1;
+        
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            
+            int row = mid / n;
+            int col = mid % n;
+            
+            if(matrix[row][col] == target){
+                return true;
+            }
+            
+            if(matrix[row][col] < target){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        
+        return false;
+    }
 }
