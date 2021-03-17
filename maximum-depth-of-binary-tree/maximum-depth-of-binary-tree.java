7557240363333571ea3d275490ba956a733de9f1
Lohith Aswathappa <47034766+Lohith-Aswathappa@users.noreply.github.com>
2021-03-17 11:34:12 -0500

    Time: 0 ms (100.00%), Space: 38.7 MB (85.92%) - LeetHub

--- maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -1,40 +1,38 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    int maxi;
-    public int maxDepth(TreeNode root) {
-        // edge case
-        if(root == null){
-            return 0;
-        }
-        //logic
-        maxi = 0;
-        dfs(root, 0);
-        
-        return maxi+1;
-    }
-    
-    private void dfs(TreeNode root, int depth){
-        //base case
-        if(root == null){
-            return;
-        }
-        //logic
-        maxi = Math.max(depth, maxi);
-        dfs(root.left, depth+1);
-        dfs(root.right, depth+1);
-    }
+    int depth;
+    public int maxDepth(TreeNode root) {
+        depth = 0;
+        if(root == null){
+            return depth;
+        }
+        dfs(root, 0);
+        return depth+1;
+    }
+    
+    private void dfs(TreeNode root, int level){
+        //base case
+        if(root == null){
+            return;
+        }
+        //logic
+        depth = Math.max(depth, level);
+        
+        dfs(root.left,level+1);
+        dfs(root.right, level+1);
+    }
 }
