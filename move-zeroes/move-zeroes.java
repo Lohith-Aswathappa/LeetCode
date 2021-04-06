2f632cc162583a0adab325b5e3d62abbfa242596
Lohith Aswathappa <47034766+Lohith-Aswathappa@users.noreply.github.com>
2021-04-06 11:34:36 -0500

    Time: 0 ms (100.00%), Space: 39.3 MB (49.69%) - LeetHub

--- move-zeroes/move-zeroes.java
+++ move-zeroes/move-zeroes.java
@@ -1,21 +1,15 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
-        //edge case
-        if(nums == null || nums.length == 0)
-            return;
-        
-        //logic
-        
-        int index = 0;
-        
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] != 0){
-                nums[index++] = nums[i];
-            }
-        }
-        
-        while(index < nums.length){
-            nums[index++] = 0;
-        }
-    }
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                nums[j++] = nums[i];
+            }
+        }
+        
+        while(j < nums.length){
+            nums[j++] = 0;
+        }
+    }
 }
